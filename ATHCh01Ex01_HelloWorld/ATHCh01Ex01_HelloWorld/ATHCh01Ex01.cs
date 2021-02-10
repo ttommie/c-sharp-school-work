f879be85426e81b2ed6b4f9d5b8f9306687f6964
Tommy <62820055+ttommie@users.noreply.github.com>
2021-02-10 10:57:12 -0500

    Add files via upload

---
+++ ATHCh01Ex01_HelloWorld/ATHCh01Ex01_HelloWorld/ATHCh01Ex01.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Programmer: Tommie
+ * Date: 01/23/2021
+ * Project: Hello World 
+ * Chapter: 01 Ex: 01
+ */
+
+//DIRECTIVES 
+using System;
+using static System.Console;
+
+namespace ATHCh01Ex01_HelloWorld
+{
+    class ATHCh01Ex01
+    {
+        static void Main()
+        {
+            //PRINT OUT THE MESSAGE TO THE CONSOLE
+            WriteLine("Hello World! My name is Aaron Hearne");
+            //WAIT FOR USER TO HIT A KEY ON KEYBOARD TO CONTINUE
+            ReadKey();
+        }
+    }
+}
