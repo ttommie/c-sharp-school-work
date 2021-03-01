5ba799429b029bfb5dc6ca89c4da92ecddbeb475
Tommy <62820055+ttommie@users.noreply.github.com>
2021-03-01 18:09:41 -0500

    Add files via upload

---
+++ ATHCh04_MotorwayApp/ATHCh04_MotorwayApp/Motorway.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+/*
+ * Programmer: Tommie
+ * Date: 03/01/2021
+ * Desc: Instantiable Class
+ * Data Members: Name | Type | Direction | Surface | # of Lanes | Toll | Party |
+ */
+
+
+namespace ATHCh04_MotorwayApp
+{
+    class Motorway
+    {
+        //INSTANCE VARIABLES 
+        private string nameOfMotorway,
+            typeOfMotorway,
+            surface,
+            mntceParty,
+            direction;
+        private int numberOfLanes;
+        private bool toll;
+
+        //DEFAULT CONSTRUCTOR 
+        public Motorway()
+        {
+
+        }
+
+        //MOST COMMON CONSTRUCTOR 
+        public Motorway(string mwName, string mwType, string mwSurface, int mwNbrLanes, string mwMntceParty)
+        {
+            nameOfMotorway = mwName;
+            typeOfMotorway = mwType;
+            surface = mwSurface;
+            numberOfLanes = mwNbrLanes;
+            mntceParty = mwMntceParty;
+        }
+
+        //ALL VARIABLE CONSTRUCTOR 
+        public Motorway(string mwName, string mwType, string mwSurface, int mwNbrLanes,bool mwToll, string mwMntceParty)
+        {
+            nameOfMotorway = mwName;
+            typeOfMotorway = mwType;
+            surface = mwSurface;
+            numberOfLanes = mwNbrLanes;
+            toll = mwToll;
+            mntceParty = mwMntceParty;
+        }
+
+        //PROPERTIES 
+        public string NameOfMotorway
+        {
+            get
+            {
+                return nameOfMotorway;
+            }
+            set
+            {
+                nameOfMotorway = value;
+            }
+        }
+
+        public string TypeOfMotorway
+        {
+            get
+            {
+                return typeOfMotorway;
+            }
+            set
+            {
+                typeOfMotorway = value;
+            }
+        }
+
+        public string Surface
+        {
+            get
+            {
+                return surface;
+            }
+            set
+            {
+                surface = value;
+            }
+        }
+
+        public string Direction
+        {
+            get
+            {
+                return direction;
+            }
+            set
+            {
+                direction = value;
+            }
+        }
+
+        public int NumberOfLanes
+        {
+            get
+            {
+                return numberOfLanes;
+            }
+            set
+            {
+                numberOfLanes = value;
+            }
+        }
+
+        public bool Toll
+        {
+            get
+            {
+                return toll;
+            }
+            set
+            {
+                toll = value;
+            }
+        }
+
+        public string MntceParty
+        {
+            get
+            {
+                return mntceParty;
+            }
+            set
+            {
+                mntceParty = value;
+            }
+        }
+
+        //INSTANCE METHODS 
+        public string GetFullName()
+        {
+            return "Name of Motorway: " + nameOfMotorway + " " + typeOfMotorway + " " + direction;
+        }
+
+        public string GetTollInformation()
+        {
+            return GetFullName() + "\n" + "Toll: " + toll;
+        }
+        public string GetLaneInformation()
+        {
+            return GetFullName() + "\n" + "Number of Lanes: " + numberOfLanes;
+        }
+
+        //INSTANCE METHOD INHERITED FROM OBJECT
+        public override string ToString()
+        {
+            return GetFullName() +
+                "\nToll: " + toll +
+                "\nNumber of Lanes: " + numberOfLanes +
+                "\nType of Surface: " + surface +
+                "\nMaintained by: " + mntceParty;
+        }
+    }
+}
+ 
---
+++ ATHCh04_MotorwayApp/ATHCh04_MotorwayApp/MotorwayProgram.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Programmer: Tommie
+ * Date: 03/01/2021
+ * Project: Motorway Application
+ * Chapter: 04
+ */
+
+//DIRECTIVES
+using System;
+using static System.Console;
+
+namespace ATHCh04_MotorwayApp
+{
+    class MotorwayProgram
+    {
+        static void Main()
+        {
+            Motorway mwObject; //DECLARE BUT NOT INSTANTIATED (NO OBJECT CREATED YET)
+
+            //VARIABLES
+            string nameOfMW,
+                typeOfMW,
+                surfaceOfMW,
+                mntcePartyForMW;
+            int lanesforMW;
+
+            //FIRST OBJECT
+            Motorway hwy401 = new Motorway("HWY 401", "Arterial", "Asphalt", 4, "State");
+            hwy401.Toll = false;
+            hwy401.Direction = "North/South";
+            WriteLine("First Test\n" + hwy401 + "\n"); //NOTICE OBJECT hwy401 IN WRITELINE CAUSES TOSTRING() METHOD TO EXECUTE
+
+            //SECOND OBJECT
+            Motorway secondTest = new Motorway();
+            secondTest.NameOfMotorway = "College Circle";
+            secondTest.TypeOfMotorway = "Local";
+            secondTest.MntceParty = "Private";
+            secondTest.Direction = " ";
+            secondTest.Toll = false;
+            secondTest.Surface = "Asphalt";
+            WriteLine("Second Test\n" + secondTest + "\n"); //NOTICE OBJECT secondTest IN WRITELINE CAUSES TOSTRING() METHOD TO EXECUTE
+
+            //THIRD OBJECT 
+            Motorway thirdTest = new Motorway();
+            WriteLine("Third Test - No Values Assigned");
+            //NOTICE USE OF PROPERTIES OF OBJECT
+            WriteLine("Motorway Test 3: " + thirdTest.NameOfMotorway + " " +
+                thirdTest.TypeOfMotorway + " " + thirdTest.Direction + " " +
+                "\nLanes: " + thirdTest.NumberOfLanes +
+                "\nTolls: " + thirdTest.Toll +
+                "\nMaintained By: " + thirdTest.MntceParty);
+
+            //CLEAR SCREEN OF PAST TEST
+            ReadKey();
+            Clear(); 
+
+            //ASK FOR DATA AND ASSIGN TO VARIABLES
+            nameOfMW = GetStrInput("Enter Name of Motorway: ");
+            typeOfMW = GetStrInput("Enter Type of Motorway: ");
+            surfaceOfMW = GetStrInput("Enter Surface of Motorway: ");
+            lanesforMW = GetIntInput("Enter Number of Lanes for Motorway: ");
+            mntcePartyForMW = GetStrInput("Enter Maintenance Party of Motorway: ");
+
+            //CREATE (INSTANTIATE) mwObject WITH INTIAL VALUES USING INPUT VARIABLES
+            mwObject = new Motorway(nameOfMW, typeOfMW, surfaceOfMW, lanesforMW, mntcePartyForMW);
+
+            //SET PROPERTIES AFTER CREATION
+            mwObject.Toll = false;
+            mwObject.Direction = " ";
+
+            WriteLine(); //BLANK LINE
+            WriteLine($"Motorway mwObject \n Name:" + mwObject.GetFullName() +
+                $"\nSurface: {mwObject.Surface} \nNumber of Lanes: {mwObject.NumberOfLanes}" +
+                $"\nToll: {mwObject.Toll} \nMaintenance: {mwObject.MntceParty} ");
+
+            WriteLine(mwObject.GetLaneInformation());
+
+
+            //WAIT FOR USER INPUT TO EXIT THE PROGRAM
+            ReadKey();
+        }
+
+        public static int GetIntInput(string prompt)
+        {
+            //LOCAL VARIABLES 
+            string inputValue;
+            int intValue;
+
+            Write($"{prompt}");
+            inputValue = ReadLine();
+            intValue = int.Parse(inputValue);
+
+            return intValue;
+        }
+
+        public static string GetStrInput(string prompt)
+        {
+            //LOCAL VARIABLES 
+            string inputValue;
+
+            Write($"{prompt}");
+            inputValue = ReadLine();
+
+            return inputValue;
+        }
+    }
+}
