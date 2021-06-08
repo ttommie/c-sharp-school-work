dd038ee7f900cab723c59b92754ffab7b0a3a023
Tommie <62820055+ttommie@users.noreply.github.com>
2021-06-08 13:35:38 -0400

    Add files via upload

---
+++ ATHFinalExam_GallonsNeeded/ATHFinalExam_GallonsNeeded/ATHFinalExam.cs
@@ -0,0 +1,221 @@
+﻿/*
+ * Programmer: Tommie 
+ * Date: 05/07/2021
+ * File: ATHFinalExam.cs
+ * Project: Final Exam - Gallons Needed to Travel
+ */
+
+//DIRECTIVES
+using System;
+using static System.Console;
+using System.Threading;
+
+namespace ATHFinalExam_GallonsNeeded
+{
+    class ATHFinalExam
+    {
+        static void Main()
+        {
+            //VARAIBLES 
+            int traveling, 
+                miles;
+
+            string sendOverride;
+
+            bool doAgain = false;
+
+            //DISPLAY INSTURCTIONS
+            DisplayInstructions();
+
+            do
+            {
+                //GET MILES TRAVELING
+                GetMilesTraveling(out traveling);
+
+                // GET MILES PER GALLON
+                GetMilesPerGallon(out miles);
+
+                //OBJECT VARIABLES | CONSTURCTORS & PROPERTIES 
+                Traveling consturctorTest = new Traveling(traveling, miles);
+
+                //SET STRING TO SEND TO DISPLAY RESULTS METHOD FOR CONSTURCTOR OBJECT
+                sendOverride = consturctorTest.ToString();
+
+                //DISPlAY RESULTS 
+                DisplayResults(sendOverride);
+
+                //ASK IF THEY WOULD LIKE TO ENTER ANOTHER VALUE
+                doAgain = WhileAgain();
+
+                if (doAgain == false)
+                {
+                    //IF USER ENTERS NO , CLEAR SCREEN
+                    Clear();
+
+
+                }
+
+            } while (doAgain);
+
+            Traveling propertiesTest = new Traveling();
+            propertiesTest.MilesToTravel = 543;
+            propertiesTest.MilesPerGallon = 21;
+
+            //SET STRING TO SEND TO DISPLAY RESULTS METHOD FOR PROPERTIES OBJECT
+            sendOverride = propertiesTest.ToString();
+
+            //DISPLAY PROPERTIES TEST
+            WriteLine("Properties Test Object | Miles To Travel: 543 | Miles Per Gallon: 21");
+            WriteLine("Press Any Key to See Results...");
+
+            //WAIT FOR USER INPUT TO DISPLAY RESULTS
+            ReadKey();
+
+            //DISPLAY RESULTS
+            DisplayResults(sendOverride);
+
+            //DISPLAY A GOODBYE MESSAGE TO USER
+            WriteLine("GoodBye!");
+            System.Threading.Thread.Sleep(2000);
+
+        }
+
+        //METHODS 
+
+        static void DisplayInstructions()
+        {
+            //DISPLAY INSTURCTIONS
+            WriteLine("+----------------------------------------------+");
+            WriteLine("Inputs: Miles Traveling & Miles Per Gallon");
+            WriteLine("Results: Fuel Needed to Arrive to Destination");
+            WriteLine("+----------------------------------------------+");
+            WriteLine("Press Any Key to Proceed...");
+
+            //WAIT FOR USER INPUT TO PROCEED
+            ReadKey();
+
+            //CLEAR SCREEN AFTER USER PRESSES KEY
+            Clear();
+        }
+
+
+        static void GetMilesTraveling(out int traveling)
+        {
+            //LOCAL VARIABLE
+            string userValue = "";
+
+            //ASK QUESTION
+            WriteLine("How Many Miles Will You be Traveling:");
+
+            //READ USER INPUT BEFORE LOOP
+            userValue = ReadLine();
+
+            //LOOP THROUGH FOR ERROR CHECKING 
+            while (int.TryParse(userValue, out traveling) == false || traveling < 0)
+            {
+                //IF USER ENTERS A NON NUMERIC NUMBER 
+                if (int.TryParse(userValue, out traveling) == false)
+                {
+                    Clear();
+                    WriteLine("Error: Non-Numeric Value Entered....");
+                    WriteLine("How Many Miles Will You be Traveling:");
+                    userValue = ReadLine();
+                }
+                else if (traveling < 0) //IF USER ENTERS A NEGATIVE NUMBER  
+                {
+                    Clear();
+                    WriteLine("Error: Negative Value Entered....");
+                    WriteLine("How Many Miles Will You be Traveling:");
+                    userValue = ReadLine();
+                }
+            }
+        }
+
+        static void GetMilesPerGallon(out int miles)
+        {
+            //LOCAL VARIABLE
+            string userValue = "";
+
+            //ASK QUESTION
+            WriteLine("How Many Miles do You Get to The Gallon:");
+
+            //READ USER INPUT BEFORE LOOP
+            userValue = ReadLine();
+
+            //LOOP THROUGH FOR ERROR CHECKING
+            while(int.TryParse(userValue, out miles) == false || miles < 0 || miles < 5 || miles > 75)
+            {
+                //USER ENTERS A NON NUMERIC NUM
+                if (int.TryParse(userValue, out miles) == false)
+                {
+                    Clear();
+                    WriteLine("Error: Non-Numeric Value Entered....");
+                    WriteLine("How Many Miles do You Get to The Gallon:");
+                    userValue = ReadLine();
+                }
+                else if (miles < 0) //USER ENTERS A NEGATIVE NUMBER 
+                {
+                    Clear();
+                    WriteLine("Error: Non-Numeric Value Entered....");
+                    WriteLine("How Many Miles do You Get to The Gallon:");
+                    userValue = ReadLine();
+                }
+                else if (miles < 5 || miles > 75) //USER ENTERS A OUT OF RANGE NUMBER
+                {
+                    Clear();
+                    WriteLine("Error: Out of Range Value Entered.... Range: 5-75");
+                    WriteLine("How Many Miles do You Get to The Gallon:");
+                    userValue = ReadLine();
+                }
+            }
+        }
+
+        static void DisplayResults(string strOverride)
+        {
+            //CLEAR SCREEN TO DISPLAY RESULTS 
+            Clear();
+
+            //DISPLAY RESULTS TO USER
+            WriteLine("Results");
+            WriteLine("+---------------------------------------------------------------------------+");
+            WriteLine(strOverride);
+            WriteLine("+---------------------------------------------------------------------------+");
+            WriteLine("Pres Any Key to Proceed...");
+
+            //WAIT FOR USER INPUT TO PROCEED
+            ReadKey();
+        }
+
+        static bool WhileAgain()
+        {
+            //LOCAL VARIABLES 
+            bool again = true;
+            string userValue;
+            
+            //ASK QUESTION
+            WriteLine("Would You Like to Enter Another Value? [Y/N]: ");
+            userValue = ReadLine().ToUpper();
+
+            //ERROR CHECKING IF USER ENTERED Y OR N
+            while (userValue != "Y" && userValue != "N")
+            {
+                Clear();
+                WriteLine("Error: Value must be a Y or N....");
+                WriteLine("Would You Like to Enter Another Value? [Y/N]: ");
+                userValue = ReadLine().ToUpper();
+            }
+
+            //SET AGAIN DEPENDING ON USER INPUT
+            if (userValue == "Y")
+            { again = true; }
+            else if (userValue == "N")
+            { again = false; }
+
+            //CLEAR SCREEN AFTER ASKING USER 
+            Clear();
+
+            return again;
+        }
+
+    }
+}
---
+++ ATHFinalExam_GallonsNeeded/ATHFinalExam_GallonsNeeded/Traveling.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Programmer: Tommie
+ * Date: 05/07/2021
+ * File: Traveling.cs
+ * Project: Final Exam - Gallons Needed to Travel
+ */
+
+//DIRECTIVES 
+using System;
+using static System.Math;
+
+namespace ATHFinalExam_GallonsNeeded
+{
+    class Traveling
+    {
+        //INSTANCE VARIABLES 
+        private int
+            milesToTravel,
+            milesPerGallon;
+
+        private double fuelNeeded;
+
+        //CONSTURCTORS
+        public Traveling()
+        {
+            //DEFAULT CONSTURCTOR
+        }
+
+        public Traveling(int milesTraveling, int MPG)
+        {
+            milesToTravel = milesTraveling;
+            milesPerGallon = MPG;
+        }
+
+        //PROPERIES 
+        public int MilesToTravel
+        {
+            get { return milesToTravel; }
+            set { milesToTravel = value; }
+        }
+        public int MilesPerGallon
+        {
+            get { return milesPerGallon; }
+            set { milesPerGallon = value; }
+        }
+
+        public double FuelNeeded
+        {
+            get { return fuelNeeded; }
+        }
+
+
+        //INSTANCE METHODS 
+        public double CalculateFuel()
+        {
+            return fuelNeeded = Math.Round(((double)milesToTravel / milesPerGallon));
+        }
+
+        public override string ToString()
+        {
+            return $"You will need approximately {CalculateFuel()} gallons of fuel to travel {milesToTravel} miles at {milesPerGallon} MPG. ";
+        }
+    }
+}
